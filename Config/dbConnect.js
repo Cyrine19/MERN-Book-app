993217a9b0640743d84c24f73c215a5d6acfe751
cyrine19 <syrineba9@gmail.com>
2023-10-01 11:41:51 +0100

    first commit

---
+++ Config/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require ("mongoose");
+
+const dbConnect = () => {
+    //connect DB
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology :true,       
+})
+.then(()=> console.log('Db Connected'))
+.catch(err => console.log(err));
+};
+
+module.exports = dbConnect ;
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ('express') ;
+const dotenv = require ('dotenv');
+const error = require ('./middlewares/errorMiddlewareHandler.js');
+const userRoute = require ('./routes/usersRoutes.js');
+const dbConnect =require ('./Config/dbConnect.js');
+const usersRoute = require('./routes/usersRoutes.js');
+dotenv.config();
+const app = express();
+
+//Db connect 
+dbConnect();
+//Passing body data
+app.use(express.json ());
+
+//Routes
+app.use('/api/users', usersRoute);
+console.log((process.env.MY_NAME));
+//Error middleware
+app.use (error.errorMiddlewareHandler);
+
+
+
+//Server
+const PORT = process.env.PORT || 5001;
+ 
+app.listen(5001, () => {
+    console.log('Server is running on port:' , 5001)
+});
+//mongodb+srv://syrineba9:<password>@cluster0.kkssldn.mongodb.net/
